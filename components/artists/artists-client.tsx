0338c29d26301af9862921c525eb5c57ae4aac3d
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-10-14 14:51:58 +0200

    feat: added category filter to the artists page

--- components/artists/artists-client.tsx
+++ components/artists/artists-client.tsx
@@ -6,12 +6,15 @@ import FilterSheetWrapper from "@/components/book/filter-sheet-wrapper";
 import SearchInput from "@/components/(menu)/book/search-input";
 import ArtistList from "@/components/artists/artist-list";
 import { useState, useMemo, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import { Users } from "lucide-react";
+import CategorySelector from "../book/category-selector";
+import { Category } from "@/definitions/services";
 
 type ArtistsClientProps = {
   initialCategory: string;
   artists: any[];
-  categories: { name: string }[];
+  categories: Category[];
 };
 
 export default function ArtistsClient({
@@ -27,9 +30,16 @@ export default function ArtistsClient({
     duration: "Any",
   });
 
+  const router = useRouter();
+
   useEffect(() => {
     setCategory(initialCategory);
   }, [initialCategory]);
+
+  const handleCategoryChange = (cat: string) => {
+    setCategory(cat);
+    router.replace(`/artists?category=${encodeURIComponent(cat)}`);
+  };
 
   const filteredArtists = useMemo(() => {
     let list = [...artists];
@@ -70,27 +80,34 @@ export default function ArtistsClient({
       <div className="w-full md:w-[390px] h-[844px] bg-white md:rounded-[2.5rem] md:shadow-2xl md:overflow-hidden relative">
         <div className="px-4 pt-6 pb-24 overflow-y-auto h-full">
           {/* Header */}
-          <div className="absolute top-0 left-0 right-0 bg-white flex flex-col gap-2 px-4 pt-4 pb-2 z-10">
+          <div className="absolute top-0 left-0 right-0 bg-white flex flex-col gap-2 px-4 pt-4 pb-0 z-10">
             <div className="flex items-center space-x-3">
               <div className="p-2 rounded-full bg-white shadow">
                 <Users className="w-5 h-5 text-gray-600" />
               </div>
-
               <div className="flex items-center justify-center">
                 <p className="font-bold text-lg"> Artists</p>
               </div>
             </div>
 
+            {/* Search and Filters */}
             <SearchInput
               query={query}
               setQuery={setQuery}
               onOpenFilters={() => setFilterOpen(true)}
             />
 
+            {/* Category Selector */}
+            <CategorySelector
+              categories={categories}
+              currentCategory={category}
+              onSelectCategory={handleCategoryChange}
+            />
+
             <ActiveFilters filters={filters} clearFilter={clearFilter} />
           </div>
 
-          <div className="mt-26" />
+          <div className="mt-38" />
 
           <FeaturedOfferWrapper />
 
